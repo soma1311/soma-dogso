db5632ec3845388e4dad6ef0451fe3da62f45c8c
soma1311 <87514136+soma1311@users.noreply.github.com>
2025-03-17 11:29:37 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd /var/www/dogbreedshow
+cd /opt/dogbreedshow
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Install dependencies (optional, if not already installed)
+pip install -r requirements.txt
+
+# Start the Flask application
 python3 app.py &
